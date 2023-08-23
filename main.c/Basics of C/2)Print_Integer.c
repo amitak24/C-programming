206d8abd2a6a835f917a22930db61dd8034095c7
amitak24 <142614018+amitak24@users.noreply.github.com>
2023-08-23 17:39:54 +0530

    Add files via upload

---
+++ main.c/Basics of C/2)Print_Integer.c
@@ -0,0 +1,14 @@
+//Write a Program to Print Integer Number Entered by User
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("ENTER VALUE FOR a:");
+    scanf("%d",&a);
+
+    printf("THE OF a=%d",a);
+
+
+    return 0;
+}
---
+++ main.c/Basics of C/7)ASCII_value.c
@@ -0,0 +1,13 @@
+// Write a Program to Find ASCII Value of a Character
+
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("enter any character:");
+    scanf("%c",&ch);
+
+    printf("the ASCII value of %c is %d",ch,ch);
+
+    return 0;
+}
